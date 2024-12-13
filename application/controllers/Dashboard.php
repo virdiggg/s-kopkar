97c8b1ecd881781d42699d96c9fa601f48113457
Abang Benerin <virdigunawann@gmail.com>
2024-12-13 22:56:16 +0700

    fix type

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,6 +6,17 @@ class Dashboard extends CI_Controller {
 	
 	public function index()
 	{
+        // $this->load->model('user_m');
+        // $this->load->library('authentication');
+        // $param = [
+        //     'username' => 'testing',
+        //     'password' => '123456',
+        // ];
+
+        // $query = $this->user_m->login($param);
+        // $user = $query->row();
+        // $token = $this->authentication->generateJWTToken($user);
+		// echo "<pre>";var_dump($user);var_dump($token);die;
 		// $this->load->view('dashboard');
 		check_not_login();
 		
