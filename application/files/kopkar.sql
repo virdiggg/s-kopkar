07ac64a700b202c96eeac938db896acf3a9ee9d7
Abang Benerin <virdigunawann@gmail.com>
2024-12-23 15:28:38 +0700

    db

--- application/files/kopkar.sql
+++ application/files/kopkar.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 20/12/2024 22:12:36
+ Date: 23/12/2024 15:28:27
 */
 
 SET NAMES utf8mb4;
@@ -47,7 +47,7 @@ CREATE TABLE `tb_anggota`  (
 -- Records of tb_anggota
 -- ----------------------------
 INSERT INTO `tb_anggota` VALUES (1, '3000', 'ADMIN', '2023-10-06', '', '2023-10-06', '06001', '', '', 'admin', 'f865b53623b121fd34ee5426c792e5c33af8c227', '1', '2023-10-06 16:03:13', '2024-12-16 18:32:23', '1', 'eyJpdiI6ImlOZ0tFa1VsQlE0bElMRHIiLCJ2YWx1ZSI6InhkSGZ3RmxOQXphN0hvQTlmRnk1VUpNOSs5dElBbmNhRFZPTEdhK2dyaCtXRFNobW1vSnhlNCt2UXdSK1NranZBVmYvZTdkY3ZQVzN3OG5PL1ZRQjBaUklaR0JibEM1aEplM29YMmk3WHM3QTRYd0dCN3hwWnRSeWRrcjM3RElrMk9uclEyZlRsdmo1YkRXVm5iN3E4UW5uSVhIYjI3a1BONHhxU1NkcUMxbUZwUTNhdmUrUzJXdE5GWWhiN21USlNQeExVTWw2ZVJucUc1MUlhMFJLTzBxRkw2blk3NW81RzFzRGNkcFNDT0pzL1JxU3ZmQXk4eEFzdXB5MnJGTWQ3YXRtV2ZUM2cxTVlKUVEreFNkQ1Z0MUpYQnJBbCs0ZzZoWlRuRjh1cERpWVZIRzlPYU9FL3dzR2lsL21BaVU0WmJhVVh5a0NRdU5lQXllZU41RU12NVhJbjhXeHplZDR2UWNGOTZyTmxYckdSeTNLSy9BZ09pMTh0OFFoOGx3dUQ4UlFTMnhhMkRFcm1td21ZU2pYV2VkRnFEejFLNTRacWNyMkxEdEpJUS9NK1Y4YW42SG4waGFxakhSMjlYM3RMTW5sczZUTTBPbFpTQ0QrbUlYNnUrT3dqbHl0SjdacEdRZ1A0SzVmdUhQYS9NQU9XT1RIQlN0ZjROcUM3d2F0VXExbTRiZW9KVXp2dVFUWU8xQWF4MEo1c1VUZzdnQzZQL2gySit0SiIsIm1hYyI6IiIsInRhZyI6ImVVZlpmUGxOMU5oSzRwRXhJNi9LcWc9PSJ9');
-INSERT INTO `tb_anggota` VALUES (128, '3001', 'RIDWAN HAKIM', '2007-02-06', 'DEPOK', '2023-10-06', '06000', 'BANK BCA', '0381398183', 'ridwan', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2', '2023-10-06 16:22:24', '2024-12-20 19:23:41', '1', 'eyJpdiI6Ik5LeXFpVUZnWnY0UkN6YlkiLCJ2YWx1ZSI6IlEyMXp4QVFtY0JGdkc0NjJDYXJyWGoxWWk0eGJOTy8xamNybGlheEIvWGgvN0IvTTZvZUcrbzJSL2pHVDlwKzRTenJ2M3VrQXc3b3dRSWZyWjJEdEM2cjBiZ3lDMEhwNTNlbFFxYUR2NFU4OWpBNjlHVk13dko3OGhMeUhxVWRXdHVObE5HRWFkZjBvNUQzY0I4dExXc3FWbnJiYVNqV2xlVUI1SUpBZkFHVzQ3NXByRU5TRHlyRUFSc3FJdnB3U0RScTJuTnRPejY2ZGNRK3BXMWtVUHpOSmtiOFRBQkRLVit0cURRWnNOSElEVEwyaDFabjYrQXNUTHFaRTlGK1J0NDRYcEhndTlUR0xDRUx4eGRIOWlMY21BQlplOU96dTBVUjBCZVM1bnpzVTBVM0FDR3h1NTcvVGJkNzY0Zzh3dkN0U2l5dDViclZOOXd2dDBLSkVyS3hwQjF0eVd2QnhFOG9kRGZ2eXRUYW9EUUZjckFKR0R2UEFCTHRXME5WSmVQajBWN05KUE5lUCtKaEUxZ1ptZkZYbEMzVUhqK0hFU1RjTGFIOWJkK1BlY25jSWdyTHV4ck1OOXBqZ0lYU0MzYmNBL1RNK09OMmxkaitoUDExOTJ1RHlUYktvTlNEQzBINmZNaFJ4eEg3ZHFxMXdLenZBbEErNFhON29udE02MnVReVlPNzlWcUhnT0s1eUFUbWFrZ1BQVHhhcjJaLzhWK1BIT0VMTFhPSEZWNGJNVXNhUDNpeUJGemF0bUFFMTNzeTNRaXBiK2s3bnlxc3pzWHRrWmFkeTIzMGs1bVE0SUVGV1VFanlaZWs9IiwibWFjIjoiIiwidGFnIjoiUGxseHlnckhPSm5qczV2TmZYZDZTZz09In0=');
+INSERT INTO `tb_anggota` VALUES (128, '3001', 'RIDWAN HAKIM', '2007-02-06', 'DEPOK', '2023-10-06', '06000', 'BANK BCA', '0381398183', 'ridwan', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2', '2023-10-06 16:22:24', '2024-12-23 15:24:02', '1', 'eyJpdiI6IjBjdXZWc3NOWUZzdnVTZm0iLCJ2YWx1ZSI6IjN1R1dBMFZmdXRTMDRGSG9SU0k3Q3U0MFR3T2M2M0Vtcldac0VhN2NQY0g2R0xwOC9hL21vd3NxMXNLeUI2ZXNPcC9ZMGhuY3U1OUFNUXF3Y25ObHQ1UlB4VDZOdnp4aWpDTkJsYXM4cWpuR3BubXlXQmJtK2t0di9DaFpITUF4UnNiazZRVy9OYVl5SzlZMWtnU1cyU0ZxM2U3aUEvTXlwMEVQaTR3WS85RXNldmxDaytzS3RWYTVMTzJxVTROc0QvOVhZMFVEMmhEWkpNRjZ4bUFQM243aFdwU1VrVk5wMllRN2VyTExqMzJUcmovYlJwclIrNTd1ajRONUZFUDhhUmpkOHlnM294ek5DSkhTSDNieVVxdkdkNGVvM0VYaFNFVW05NUxLNDdBTGIydWc1ajNjWEJVeC9sR1dCdzVoQVJ5V2t6czVIL1YyenRLNU5oSWNhSGxheUhVc05xY3JDbW9URWdLSHZFamlhY25mQUJzQi9adlZwUmJuL0QxN05BMGp1YXltYzg1Mmd5MzVOazloYUJyYnNncHBpL3ZkSHBMbUdmUitwM2RnOWswczhhS1NFUDBteUJHbDU4QkVBODNrS0c5UzAwVWVRbjZjSEN5bGlXRCtkL0drWk51Nnk5dXJZTWJ3eTNnMmpIY3hBWHhJdTJaU092WGxKQWw0T3BmMEJBMHdNZ015SERJTGRNN3l5dWxoTU9rcnRXNW5CU2lSWW0wTWwrNFVsTXhmcmR6Q2p6Zkp4RkhkT09lR3QvWDVRbDcxaGtLTE5xaWVQYU56emZpT0gycjRXYnNieTQwNWVacFdDKzg9IiwibWFjIjoiIiwidGFnIjoiZTNjcU1teXlOMHpKOXBWOE5LMEUxZz09In0=');
 INSERT INTO `tb_anggota` VALUES (129, '3002', 'HAKIM', '2023-07-05', 'jjl', '2023-10-29', '06002', 'BANK BRI', '1352435667', 'hakim', 'f0830fa0b7cff627cc3b98df467c9a29a5097ae4', '2', '2023-10-29 05:42:00', '2024-12-16 18:32:39', '1', 'eyJpdiI6IjgzOEVWblIySGo3OXpIZUciLCJ2YWx1ZSI6Im8xQWdxZ0xKY2NOTHBwbXVmSUhSUFJWMXZqcnpMa0xmUkJOZmcvMjdFUGJyZUZDamJVVWh1Q2ZqRXBDUVFKVVJJclVDeE54amk3cy93cnJxMXFHdythVkNxcnQ3Q0dSSkZiRm9QcDJTa0s2RTllVlgvYm1BWXRQZmdMQncvVFdPamcvZHJjb25WL3dYSUhVSktWZnhtRWhmSFpVbXVwT3VTaEg4MVo5SmgwVngxTGJmaWdlRXRMeW5WQ3NGK1ExeUNyY0dXajhDa1hQQ3ZvV1FaQ0ZweHAralRDYnhnVVg3NjhQRnY5U3hKOVR1N1NBZy9IYTlIOGZPSjZvL0p3N2NGRTNmbjRaQ3RNS3grN0hkRC93RytnSmpnazU1Ny9ObkEzMGd3dThha08zODFONm5lOSt4cjdGSndEYUdETXdqb2dQYi9RYWViZGhyalFWVDY5RlhqYTJ0TjZIRkVpMjlFMXBIQmlqcW50REZmd1BRNFkwS0ppczFpekg0dkMyejNKQ3BKbVZwWWdjZ0F6d2wwWnJtV3dscEVnN0ZtUFZ4Y2VaUGtLYUVMRHo0TmlqdWxHK0lQL3ZaNkMwbnBaNUhzNm9xZnVqVEs4VHJaMzFzY3ZtNThIWldwcWJLcitYMCswOTFkZzhROHJYN28wazZGdVFqb0xFSUNEM0xERjAyZzZqOHNtd3BZak5OUnpuN0ozMFVzakxmbW5RbWJDSUtXYm9RWWNVNk1lRXpNTGI2QU9mb2ZzVGpJMm9BU281UVNtU3Y4OWt0dHZLRi9WalRPdnRhOGVKWkVkRTM2Zz09IiwibWFjIjoiIiwidGFnIjoiSXMycjd5Rm5UWmIwWDlWVXhVNzd5QT09In0=');
 INSERT INTO `tb_anggota` VALUES (132, '3003', 'TESTING', '2023-10-06', '', '2023-10-06', '06001', '', '', 'testing', '7c4a8d09ca3762af61e59520943dc26494f8941b', '1', '2023-10-06 16:03:13', '2024-12-14 20:09:44', '1', 'eyJpdiI6IjN3eTBrbHBxNWZRUUpxaFQiLCJ2YWx1ZSI6ImxlMnlHd1N0NUYxOWxIYmVaeVJnS1hsOXVKVFBabFVCZ1R1Q1Y4T0pob09VaCtsQkcxNzlWYnVzTS84dXk5djFvd291bmw0UGxnb1kzOTNYc2tPVlNMNW1lOWI2WFlaVXBWYkZhaSsvNTlyREtjWXhuR2ZkTVJmMjQ0d2JTU1AzMkJiWDZlcE5TdUJtNDdZRmFQbXR5SVpsdlBHUzA2OTArQjdjU2NaTCsxaVBsUjNMRHhySjdyQ0M3a1VGdExUZDRJc0dOdVNzd01iNlJYUkpCcENMUC95OHJiRmtjVEZmVlJRZ3NqbXg2N21zU3pvU3lsWWJQY3cyTDBMT3hZaEZvb3VzT0ZlMC9GY1dWeTFnQUdTRjBvOVVBM3RCTThCbEZ2T050VmdTTnlUZTlaTjVFZHFETCt0YkkzTzdUTEJXaWxzNVZyc2NORWxMalBBOC9UUkhzUnVBdHlyc21KdEh5aVNhZFMvOXY4Q3daOURseUQzbVFsbk13czFzcXJ6eWcvTFEwL1pFVU9lQlJWSEhBdmQxdlRjKzBZVnNWZHhRN29EMS9tLzlKa3ZtWjlpQ1dZckIzY1pLNVNZbVEwbEdrNUNXK3kyMWRyNmdqczhERWRJVGU5a2tJWUhIMEpxOVVsMUpLSU0zQm03MkFyTlRCUU1tMEFCNUhIQm0vS0w5SzVvdVlxQ1ZiMW9ZWUhXUkxGVVpvd2dWMFFBMmRNS3lmSzZCUTJYUEZjMzFReXgzbDg0c2JjSTU0Mm9BK0N2L1ZNbmFpQW89IiwibWFjIjoiIiwidGFnIjoiTUdUdmYyNzdCUm1reGFwcHpSbmFEUT09In0=');
 
@@ -87,18 +87,27 @@ CREATE TABLE `tb_kegiatan_simpanan`  (
   `pengurus` char(6) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `tanggal` date NOT NULL,
   `created` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `bukti_transfer` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
   PRIMARY KEY (`keg_simpan_id`) USING BTREE,
   INDEX `koperasi_id`(`koperasi_id` ASC) USING BTREE,
   CONSTRAINT `tb_kegiatan_simpanan_ibfk_1` FOREIGN KEY (`koperasi_id`) REFERENCES `tb_anggota` (`koperasi_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 39 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_kegiatan_simpanan
 -- ----------------------------
-INSERT INTO `tb_kegiatan_simpanan` VALUES (27, 'KK2408100001', '3001', 'Sukarela', 'Masuk', 30000000, 'Cash', '32456464', '3000', '2024-08-10', '2024-08-10 09:06:40');
-INSERT INTO `tb_kegiatan_simpanan` VALUES (28, 'KK2408100002', '3001', 'Sukarela', 'Masuk', 1234000, 'Transfer', '25252', '3000', '2024-08-10', '2024-08-10 12:12:22');
-INSERT INTO `tb_kegiatan_simpanan` VALUES (29, 'KK2412040001', '3001', 'Sukarela', 'Masuk', 1000000, 'Transfer', '123', '3000', '2024-12-04', '2024-12-04 19:10:41');
-INSERT INTO `tb_kegiatan_simpanan` VALUES (30, 'KK2412040002', '3001', 'Sukarela', 'Masuk', 10000, 'Transfer', '1122', '3000', '2024-12-04', '2024-12-04 19:12:01');
+INSERT INTO `tb_kegiatan_simpanan` VALUES (27, 'KK2408100001', '3001', 'Sukarela', 'Masuk', 30000000, 'Cash', '32456464', '3000', '2024-08-10', '2024-08-10 09:06:40', NULL);
+INSERT INTO `tb_kegiatan_simpanan` VALUES (28, 'KK2408100002', '3001', 'Sukarela', 'Masuk', 1234000, 'Transfer', '25252', '3000', '2024-08-10', '2024-08-10 12:12:22', NULL);
+INSERT INTO `tb_kegiatan_simpanan` VALUES (29, 'KK2412040001', '3001', 'Sukarela', 'Masuk', 1000000, 'Transfer', '123', '3000', '2024-12-04', '2024-12-04 19:10:41', NULL);
+INSERT INTO `tb_kegiatan_simpanan` VALUES (30, 'KK2412040002', '3001', 'Sukarela', 'Masuk', 10000, 'Transfer', '1122', '3000', '2024-12-04', '2024-12-04 19:12:01', NULL);
+INSERT INTO `tb_kegiatan_simpanan` VALUES (31, 'KK2412230001', '3001', 'Sukarela', 'Masuk', 1000000, 'Transfer', '1148535', '3001', '2024-12-23', '2024-12-23 10:27:12', NULL);
+INSERT INTO `tb_kegiatan_simpanan` VALUES (32, 'KK2412230002', '3001', 'Sukarela', 'Masuk', 1000000, 'Transfer', '1485118', '3001', '2024-12-23', '2024-12-23 10:27:24', NULL);
+INSERT INTO `tb_kegiatan_simpanan` VALUES (33, 'KK2412230003', '3001', 'Sukarela', 'Masuk', 1000000, 'Transfer', '1079724', '3001', '2024-12-23', '2024-12-23 10:27:27', NULL);
+INSERT INTO `tb_kegiatan_simpanan` VALUES (34, 'KK2412230004', '3001', 'Sukarela', 'Masuk', 1000000, 'Transfer', '1605180', '3001', '2024-12-23', '2024-12-23 10:28:16', 'bukti_transfer_3001_241223-d360f37d3d.jpg');
+INSERT INTO `tb_kegiatan_simpanan` VALUES (35, 'KK2412230005', '3001', 'Sukarela', 'Masuk', 1000000, 'Transfer', '1007936', '3001', '2024-12-23', '2024-12-23 10:45:36', 'bukti_transfer_3001_241223-1e03ef4e35.jpg');
+INSERT INTO `tb_kegiatan_simpanan` VALUES (36, 'KK2412230006', '3001', 'Sukarela', 'Masuk', 1004400, 'Transfer', '1226679', '3001', '2024-12-23', '2024-12-23 10:45:46', 'bukti_transfer_3001_241223-8437286303.jpg');
+INSERT INTO `tb_kegiatan_simpanan` VALUES (37, 'KK2412230007', '3001', 'Sukarela', 'Masuk', 15400, 'Transfer', '1127191', '3001', '2024-12-23', '2024-12-23 10:46:01', 'bukti_transfer_3001_241223-aa77985492.jpg');
+INSERT INTO `tb_kegiatan_simpanan` VALUES (38, 'KK2412230008', '3001', 'Sukarela', 'Masuk', 15400, 'Transfer', '1928162', '3001', '2024-12-23', '2024-12-23 11:01:13', 'bukti_transfer_3001_241223-1b9c1684e0.jpg');
 
 -- ----------------------------
 -- Table structure for tb_pembayaran
@@ -158,7 +167,7 @@ CREATE TABLE `tb_pengajuan`  (
   PRIMARY KEY (`pengajuan_id`) USING BTREE,
   INDEX `koperasi_id`(`koperasi_id` ASC) USING BTREE,
   CONSTRAINT `tb_pengajuan_ibfk_1` FOREIGN KEY (`koperasi_id`) REFERENCES `tb_anggota` (`koperasi_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 60 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 62 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_pengajuan
@@ -189,6 +198,8 @@ INSERT INTO `tb_pengajuan` VALUES (56, '3001', 'PJ2412160001', '2024-12-16', 50,
 INSERT INTO `tb_pengajuan` VALUES (57, '3002', 'PJ2412160002', '2024-12-16', 15000000, 12, 0, 15000000, 1250000, 'HARDLOAN', 0, 0, 0, 'MENUNGGU', '3002', NULL);
 INSERT INTO `tb_pengajuan` VALUES (58, '3001', 'PJ2412160003', '2024-12-16', 15000000, 12, 0, 15000000, 1250000, 'HARDLOAN', 0, 0, 0, 'MENUNGGU', '3001', NULL);
 INSERT INTO `tb_pengajuan` VALUES (59, '3001', 'PJ2412200001', '2024-12-20', 1000000, 5, 0, 1000000, 200000, 'HARDLOAN', 0, 0, 0, 'MENUNGGU', '3001', NULL);
+INSERT INTO `tb_pengajuan` VALUES (60, '3001', 'PJ2412230001', '2024-12-23', 1000000, 5, 0, 1000000, 200000, 'HARDLOAN', 0, 0, 0, 'MENUNGGU', '3001', NULL);
+INSERT INTO `tb_pengajuan` VALUES (61, '3001', 'PJ2412230002', '2024-12-23', 1000000, 5, 0, 1000000, 200000, 'HARDLOAN', 0, 0, 0, 'MENUNGGU', '3001', NULL);
 
 -- ----------------------------
 -- Table structure for tb_pinjaman
@@ -267,7 +278,6 @@ CREATE TABLE `tb_simpanan_sukarela`  (
   `updated` date NULL DEFAULT NULL,
   `pengurus` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `flag` enum('1','0') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '1',
-  `bukti_transfer` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
   PRIMARY KEY (`ss_id`) USING BTREE,
   UNIQUE INDEX `no_tabungan_wajib`(`no_tab_sukarela` ASC) USING BTREE,
   INDEX `koperasi_id`(`koperasi_id` ASC) USING BTREE,
@@ -277,34 +287,8 @@ CREATE TABLE `tb_simpanan_sukarela`  (
 -- ----------------------------
 -- Records of tb_simpanan_sukarela
 -- ----------------------------
-INSERT INTO `tb_simpanan_sukarela` VALUES (8, 'SS-000001', '3001', 32444000, '2023-10-18 10:43:22', NULL, '3000', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (9, 'SS-000002', '3001', 1000000, '2024-12-09 22:46:44', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (10, 'SS-000003', '3001', 1000000, '2024-12-09 23:19:40', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (11, 'SS-000004', '3001', 1000000, '2024-12-09 23:19:54', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (12, 'SS-000005', '3001', 1000000, '2024-12-09 23:20:56', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (13, 'SS-000006', '3001', 1000000, '2024-12-13 13:37:00', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (14, 'SS-000007', '3001', 36, '2024-12-13 14:02:47', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (15, 'SS-000008', '3001', 366666, '2024-12-13 14:03:35', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (16, 'SS-000009', '3001', 368600, '2024-12-13 15:21:05', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (17, 'SS-000010', '3001', 35600000, '2024-12-13 15:21:57', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (18, 'SS-000011', '3001', 3696396, '2024-12-13 15:33:06', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (19, 'SS-000012', '3001', 36369548, '2024-12-13 15:40:56', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (20, 'SS-000013', '3001', 369, '2024-12-13 15:46:20', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (21, 'SS-000014', '3001', 258069458, '2024-12-13 15:47:50', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (22, 'SS-000015', '3001', 3, '2024-12-13 15:57:28', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (23, 'SS-000016', '3001', 2147483647, '2024-12-13 15:59:07', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (24, 'SS-000017', '3001', 258490690, '2024-12-13 16:01:04', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (25, 'SS-000018', '3001', 0, '2024-12-13 20:27:22', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (26, 'SS-000019', '3001', 3, '2024-12-13 20:52:53', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (27, 'SS-000020', '3001', 36982, '2024-12-13 20:59:17', NULL, '3001', '1', NULL);
-INSERT INTO `tb_simpanan_sukarela` VALUES (28, 'SS-000021', '3003', 1000000, '2024-12-13 21:03:43', NULL, '3003', '1', 'bukti_transfer_3003_241213-b408e186c9.jpg');
-INSERT INTO `tb_simpanan_sukarela` VALUES (29, 'SS-000022', '3001', 2580, '2024-12-13 21:04:37', NULL, '3001', '1', 'bukti_transfer_3001_241213-6b7239e555.jpeg');
-INSERT INTO `tb_simpanan_sukarela` VALUES (30, 'SS-000023', '3001', 2147483647, '2024-12-14 08:55:34', NULL, '3001', '1', 'bukti_transfer_3001_241214-635433a004.jpeg');
-INSERT INTO `tb_simpanan_sukarela` VALUES (31, 'SS-000024', '3001', 2580, '2024-12-15 19:49:42', NULL, '3001', '1', 'bukti_transfer_3001_241215-3e9bff3e23.jpeg');
-INSERT INTO `tb_simpanan_sukarela` VALUES (32, 'SS-000025', '3001', 1253980, '2024-12-15 19:54:45', NULL, '3001', '1', 'bukti_transfer_3001_241215-6d4b010306.jpeg');
-INSERT INTO `tb_simpanan_sukarela` VALUES (33, 'SS-000026', '3002', 2000000, '2024-12-16 18:35:53', NULL, '3002', '1', 'bukti_transfer_3002_241216-3740dcc317.jpeg');
-INSERT INTO `tb_simpanan_sukarela` VALUES (34, 'SS-000027', '3001', 256800, '2024-12-16 21:41:00', NULL, '3001', '1', 'bukti_transfer_3001_241216-0e9c4f6971.jpeg');
-INSERT INTO `tb_simpanan_sukarela` VALUES (35, 'SS-000028', '3001', 50000, '2024-12-20 13:17:01', NULL, '3001', '1', 'bukti_transfer_3001_241220-e9c05900aa.jpeg');
+INSERT INTO `tb_simpanan_sukarela` VALUES (8, 'SS-000001', '3001', 38479200, '2023-10-18 10:43:22', '2024-12-23', '3000', '1');
+INSERT INTO `tb_simpanan_sukarela` VALUES (9, 'SS-000002', '3003', 2050504, '2024-12-09 22:46:44', '2024-12-23', '3000', '1');
 
 -- ----------------------------
 -- Table structure for tb_simpanan_wajib
@@ -344,7 +328,7 @@ CREATE TABLE `tb_swk`  (
   `tanggal` date NOT NULL,
   `pengurus` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`swk_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_swk
